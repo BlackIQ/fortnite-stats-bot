640942787de8a1829b9115e8db09ba5eab5320ba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-06 02:10:16 +0330

    New commands

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,8 @@
 import { Socks5 } from "$bot/connections/index.js";
 import { botConfig, appConfig } from "$bot/config/index.js";
-import { Bot, Stats } from "$bot/controllers/index.js";
+import { Bot, Stats, User } from "$bot/controllers/index.js";
+
+import { gateway } from "$bot/middlewares/index.js";
 
 import { Telegraf } from "telegraf";
 
@@ -10,11 +12,21 @@ const bot = new Telegraf(botConfig.token, {
   },
 });
 
+// Middlewares
+bot.use(gateway);
+
 // Bot
 bot.start(Bot.START);
 bot.help(Bot.HELP);
 
 // Stats
 bot.command("stats", Stats.STATS);
+
+// User
+bot.command("set", User.SET);
+bot.command("my", Stats.MY);
+
+// Favorites
+bot.command("favorites", (ctx) => ctx.reply("Your Favorites"));
 
 export default bot;
