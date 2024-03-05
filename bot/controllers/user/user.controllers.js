33b4a96e9e6fac99edb3c154574edb5c9ae9d236
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-06 02:09:40 +0330

    User controllers added

---
+++ bot/controllers/user/user.controllers.js
@@ -0,0 +1,29 @@
+import { API } from "$bot/api/index.js";
+
+export const SET = async (ctx) => {
+  const { _id } = ctx.fortnite.user;
+
+  try {
+    const input = ctx.message.text;
+
+    const splited = input.split(" ");
+
+    if (splited.length !== 2) {
+      return await ctx.reply("Invalid. Ex: /set GNU_Amir");
+    }
+
+    const username = splited[1];
+
+    const { data: response } = await API.patch(`users/${_id}`, {
+      fortnite_id: username,
+    });
+
+    console.log(response);
+
+    const messages = [`Username updated to ${username}`];
+
+    await ctx.reply(messages.join("\n"));
+  } catch (error) {
+    await ctx.reply(error.message);
+  }
+};
