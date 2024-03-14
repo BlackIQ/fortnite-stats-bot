df56b25d2974017e68ca1876e1547ece65b7438a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-15 03:19:18 +0330

    Export

--- bot/controllers/index.js
+++ bot/controllers/index.js
@@ -1,5 +1,6 @@
+import * as Favorite from "$bot/controllers/favorite/favorite.controllers.js";
 import * as Stats from "$bot/controllers/stats/stats.controllers.js";
 import * as User from "$bot/controllers/user/user.controllers.js";
 import * as Bot from "$bot/controllers/bot/bot.controllers.js";
 
-export { Stats, Bot, User };
+export { Stats, Bot, User, Favorite };
