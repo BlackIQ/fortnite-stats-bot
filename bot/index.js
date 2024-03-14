789718166385fdccad1a3a36ff3c7bfd6ae0fd03
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-15 03:17:52 +0330

    Update route stuff

--- bot/index.js
+++ bot/index.js
@@ -1,32 +1,36 @@
 import { Socks5 } from "$bot/connections/index.js";
 import { botConfig, appConfig } from "$bot/config/index.js";
-import { Bot, Stats, User } from "$bot/controllers/index.js";
+import { Bot, Stats, User, Favorite } from "$bot/controllers/index.js";
 
 import { gateway } from "$bot/middlewares/index.js";
 
 import { Telegraf } from "telegraf";
 
+// Set up bot instance
 const bot = new Telegraf(botConfig.token, {
   telegram: {
-    agent: !appConfig.published && Socks5,
+    agent: !appConfig.published && Socks5, // Use agent if needed
   },
 });
 
 // Middlewares
-bot.use(gateway);
+bot.use(gateway); // Everything pass throw here
 
 // Bot
-bot.start(Bot.START);
-bot.help(Bot.HELP);
+bot.start(Bot.START); // Let's start
+bot.help(Bot.HELP); // Get help
 
 // Stats
-bot.command("stats", Stats.STATS);
+bot.command("stats", Stats.STATS); // Get stat via command
+bot.action(/^stats_(.+)/, Stats.STATS); // Get stat via action
 
 // User
-bot.command("set", User.SET);
-bot.command("my", Stats.MY);
+bot.command("set", User.SET); // Set username
+bot.command("my", Stats.MY); // Get user stat
 
 // Favorites
-bot.command("favorites", (ctx) => ctx.reply("Your Favorites"));
+bot.command("favorites", Favorite.LIST); // List of them
+bot.action(/^add_(.+)/, Favorite.ADD); // Add one
+bot.action(/^remove_(.+)/, Favorite.REMOVE); // Remove one
 
 export default bot;
