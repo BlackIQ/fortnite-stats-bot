01e173cb8f82004983412cbbebcd250b62c2b548
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-15 03:19:15 +0330

    Ok

--- bot/controllers/stats/stats.controllers.js
+++ bot/controllers/stats/stats.controllers.js
@@ -1,6 +1,8 @@
-import { API } from "$bot/api/index.js";
+import { getStat } from "$bot/helper/index.js";
 
 export const STATS = async (ctx) => {
+  const { _id: user } = ctx.fortnite.user;
+
   try {
     const input = ctx.message.text;
 
@@ -12,47 +14,21 @@ export const STATS = async (ctx) => {
 
     const username = splited[1];
 
-    const { data: response } = await API.get("stats", {
-      params: {
-        name: username,
+    const { messages, buttons } = await getStat(username, user);
+
+    const buttonsMarkup = {
+      reply_markup: {
+        inline_keyboard: buttons,
       },
-    });
+    };
 
-    const data = response;
-
-    const { battlePass, image, account, stats } = data;
-    const { all, keyboardMouse, gamepad, touch } = stats;
-
-    // const messages = ["🖱 PC", "🎮 Gamepad", "📱 Touch"];
-
-    const minutesPlayed = all.overall.minutesPlayed;
-    const hoursPlayed = Math.floor(minutesPlayed / 60);
-    const remainingMinutes = minutesPlayed % 60;
-
-    const messages = [
-      // `🆔 ID: ${account.id}`,
-      `👤 Name: ${account.name}`,
-      "",
-      `⭐️ Battle pass: ${battlePass.level}`,
-      `📈 Score: ${all.overall.score}`,
-      `👊 Total mathes: ${all.overall.matches}`,
-      `🏆 Wins: ${all.overall.wins}`,
-      `🔟 Top 10: ${all.overall.top10}`,
-      `🔪 Kills: ${all.overall.kills}`,
-      `☠️ Deaths: ${all.overall.deaths}`,
-      `🧑‍🚀 K/D: ${all.overall.kd}`,
-      `⏳ Time played: ${hoursPlayed} hours and ${remainingMinutes} minutes`,
-      "",
-      "Subscribe to @telegram",
-    ];
-
-    await ctx.reply(messages.join("\n"));
+    return await ctx.reply(messages.join("\n"), buttonsMarkup);
 
     // await ctx.replyWithPhoto(image, {
     //   caption: messages.join("\n"),
     // });
   } catch (error) {
-    await ctx.reply(error.message);
+    return await ctx.reply(error.message);
   }
 };
 
@@ -66,47 +42,14 @@ export const MY = async (ctx) => {
   }
 
   try {
-    const { data: response } = await API.get("stats", {
-      params: {
-        name: fortnite_id,
-        // image: "all",
-      },
-    });
+    const messages = await getStat(fortnite_id);
 
-    const data = response;
-
-    const { battlePass, image, account, stats } = data;
-    const { all, keyboardMouse, gamepad, touch } = stats;
-
-    // const messages = ["🖱 PC", "🎮 Gamepad", "📱 Touch"];
-
-    const minutesPlayed = all.overall.minutesPlayed;
-    const hoursPlayed = Math.floor(minutesPlayed / 60);
-    const remainingMinutes = minutesPlayed % 60;
-
-    const messages = [
-      // `🆔 ID: ${account.id}`,
-      `👤 Name: ${account.name}`,
-      "",
-      `⭐️ Battle pass: ${battlePass.level}`,
-      `📈 Score: ${all.overall.score}`,
-      `👊 Total mathes: ${all.overall.matches}`,
-      `🏆 Wins: ${all.overall.wins}`,
-      `🔟 Top 10: ${all.overall.top10}`,
-      `🔪 Kills: ${all.overall.kills}`,
-      `☠️ Deaths: ${all.overall.deaths}`,
-      `🧑‍🚀 K/D: ${all.overall.kd}`,
-      `⏳ Time played: ${hoursPlayed} hours and ${remainingMinutes} minutes`,
-      "",
-      "Subscribe to @telegram",
-    ];
-
-    await ctx.reply(messages.join("\n"));
+    return await ctx.reply(messages.join("\n"));
 
     // await ctx.replyWithPhoto(image, {
     //   caption: messages.join("\n"),
     // });
   } catch (error) {
-    await ctx.reply(error.message);
+    return await ctx.reply(error.message);
   }
 };
